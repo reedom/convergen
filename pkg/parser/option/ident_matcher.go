94c7f340723e46bee0cfba5dd4c7c286377adc9e
HANAI tohru <tohru@reedom.com>
2022-09-12 16:27:20 +0900

    feat: add IdentMatcher

---
+++ pkg/parser/option/ident_matcher.go
@@ -0,0 +1,55 @@
+package option
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type IdentMatcher struct {
+	pattern   string
+	re        *regexp.Regexp
+	exactCase bool
+}
+
+func NewIdentMatcher(pattern string, exactCase bool) (*IdentMatcher, error) {
+	re, err := compileRegexp(pattern, exactCase)
+	if err != nil {
+		return nil, err
+	}
+	return &IdentMatcher{
+		pattern:   pattern,
+		re:        re,
+		exactCase: exactCase,
+	}, nil
+}
+
+func (m *IdentMatcher) Match(ident string, exactCase bool) bool {
+	if m.exactCase != exactCase {
+		m.re, _ = compileRegexp(m.pattern, exactCase)
+		m.exactCase = exactCase
+	}
+
+	s := ident
+	if !exactCase {
+		s = strings.ToLower(s)
+	}
+	return m.re.MatchString(s)
+}
+
+func compileRegexp(pattern string, exactCase bool) (*regexp.Regexp, error) {
+	var expr string
+	if strings.HasPrefix(pattern, "/") && strings.HasSuffix(pattern, "/") && 2 <= len(pattern) {
+		expr = pattern[1 : len(pattern)-1]
+	} else {
+		expr = fmt.Sprintf("^%v$", regexp.QuoteMeta(pattern))
+	}
+	if !exactCase {
+		expr = strings.ToLower(expr)
+	}
+	re, err := regexp.Compile(expr)
+	if err != nil {
+		return nil, fmt.Errorf("invalid regexp")
+	}
+	return re, nil
+}
