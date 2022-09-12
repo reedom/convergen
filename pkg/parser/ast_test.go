67db3bb9fd745dca5c009f25a70e569aecc92bd5
HANAI tohru <tohru@reedom.com>
2022-09-13 00:36:17 +0900

    test: update ast_test

--- pkg/parser/ast_test.go
+++ pkg/parser/ast_test.go
@@ -2,13 +2,29 @@ package parser
 
 import (
 	"bytes"
+	"go/ast"
+	"go/importer"
+	"go/parser"
 	"go/printer"
+	"go/token"
+	"go/types"
 	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func testLoadSrc(t *testing.T, src string) (*ast.File, *token.FileSet, *types.Package) {
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "example.go", src, parser.ParseComments)
+	require.Nil(t, err, "failed to parse test src:\n%v", src)
+
+	conf := types.Config{Importer: importer.Default()}
+	pkg, err := conf.Check("example.go", fset, []*ast.File{file}, nil)
+	require.Nil(t, err)
+	return file, fset, pkg
+}
 
 func TestGetComments(t *testing.T) {
 	t.Parallel()
@@ -22,8 +38,8 @@ var x = 0
 // Comment I-1
 // convergen:command i
 // Comment I-2
-type convergen interface {
-    // Comment M-1
+type Convergen interface {
+   // Comment M-1
 	// convergen;command m
 	// Comment M-2
 	ToModel()
@@ -32,12 +48,11 @@ type convergen interface {
 // y is.
 var y = 0
 `
-	p, err := NewParser("comment.go", src)
-	require.Nil(t, err)
-	intf, err := p.entryFile.getInterface()
-	require.Nil(t, err)
+	file, _, pkg := testLoadSrc(t, src)
+	intf := findInterface(pkg.Scope(), "Convergen")
+	require.NotNil(t, intf)
 
-	found := astGetDocCommentOn(p.entryFile.file, intf)
+	found := astGetDocCommentOn(file, intf)
 	require.NotNil(t, found)
 	assert.Len(t, found.List, 3)
 	assert.Equal(t, "// Comment I-1", found.List[0].Text)
@@ -52,20 +67,19 @@ package main
 // x is.
 var x = 0
 
-type convergen interface {
-    // Comment M-1
+type Convergen interface {
+   // Comment M-1
 	ToModel()
 }
 
 // y is.
 var y = 0
 `
-	p, err := NewParser("comment.go", src)
-	require.Nil(t, err)
-	intf, err := p.entryFile.getInterface()
-	require.Nil(t, err)
+	file, _, pkg := testLoadSrc(t, src)
+	intf := findInterface(pkg.Scope(), "Convergen")
+	require.NotNil(t, intf)
 
-	found := astGetDocCommentOn(p.entryFile.file, intf)
+	found := astGetDocCommentOn(file, intf)
 	assert.Nil(t, found)
 }
 
@@ -95,11 +109,9 @@ var y = 1
 `
 	re := regexp.MustCompile(`//\s*remove\b`)
 
-	p, err := NewParser("comment.go", src)
-	require.Nil(t, err)
-	e := p.entryFile
-	astRemoveMatchComments(e.file, re)
+	file, fset, _ := testLoadSrc(t, src)
+	astRemoveMatchComments(file, re)
 	buf := bytes.Buffer{}
-	require.Nil(t, printer.Fprint(&buf, e.fileSet, e.file))
+	require.Nil(t, printer.Fprint(&buf, fset, file))
 	assert.Equal(t, expected, buf.String())
 }
