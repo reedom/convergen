fe39ffc175f52c30098fe417b4512f5320cf7b7f
HANAI tohru <tohru@reedom.com>
2022-09-12 16:53:46 +0900

    feat: add FieldConverter

---
+++ pkg/parser/option/field_converter.go
@@ -0,0 +1,32 @@
+package option
+
+type FieldConverter struct {
+	m         *FieldMatcher
+	converter string
+	hasError  bool
+}
+
+func NewFieldConverter(src, dst string, exactCase bool, converter string, hasError bool) (*FieldConverter, error) {
+	m, err := NewFieldMatcher(src, dst, exactCase)
+	if err != nil {
+		return nil, err
+	}
+
+	return &FieldConverter{
+		m:         m,
+		converter: converter,
+		hasError:  hasError,
+	}, nil
+}
+
+func (c *FieldConverter) Match(src, dst string, exactCase bool) bool {
+	return c.m.Match(src, dst, exactCase)
+}
+
+func (c *FieldConverter) Converter() string {
+	return c.converter
+}
+
+func (c *FieldConverter) HasError() bool {
+	return c.hasError
+}
