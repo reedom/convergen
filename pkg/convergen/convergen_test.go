12d11dd0267a32f750fc6bcaab536715b523207c
HANAI tohru <tohru@reedom.com>
2022-09-15 22:34:47 +0900

    refactor: move test

--- pkg/convergen/convergen_test.go
+++ pkg/convergen/convergen_test.go
@@ -3,41 +3,11 @@ package convergen
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestParser_Parse(t *testing.T) {
 	p, err := NewConvergen("../fixtures/setups/getter/setup.go")
 	require.Nil(t, err)
-	require.Nil(t, p.Parse())
-}
-
-func TestPathMatch(t *testing.T) {
-	t.Parallel()
-	cases := []struct {
-		pattern   string
-		path      string
-		exactCase bool
-		match     bool
-	}{
-		{"Name", "Name", true, true},
-		{"Name", "Nam", true, false},
-		{"Name", "name", true, false},
-		{"name", "Name", true, false},
-		{"name", "Name", false, true},
-		{"Name", "name", false, true},
-		{"Na*", "name", false, true},
-		{"N*e", "name", false, true},
-	}
-
-	for i, tt := range cases {
-		actual, err := pathMatch(tt.pattern, tt.path, tt.exactCase)
-		require.Nil(t, err, `case %v has invalid pattern "%v"`, i, tt.pattern)
-		if tt.match {
-			assert.True(t, actual, `pattern "%v" against "%v" should match`)
-		} else {
-			assert.False(t, actual, `pattern "%v" against "%v" should not match`)
-		}
-	}
+	require.Nil(t, p.ExtractIntfEntry())
 }
--- pkg/convergen/types_test.go
+++ pkg/convergen/types_test.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"go/types"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestWalkStruct(t *testing.T) {
@@ -34,4 +37,33 @@ type Category struct {
 	}
 
 	walkStruct("v", pkg, obj.Type(), cb, opt)
+}
+
+func TestPathMatch(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		pattern   string
+		path      string
+		exactCase bool
+		match     bool
+	}{
+		{"Name", "Name", true, true},
+		{"Name", "Nam", true, false},
+		{"Name", "name", true, false},
+		{"name", "Name", true, false},
+		{"name", "Name", false, true},
+		{"Name", "name", false, true},
+		{"Na*", "name", false, true},
+		{"N*e", "name", false, true},
+	}
+
+	for i, tt := range cases {
+		actual, err := pathMatch(tt.pattern, tt.path, tt.exactCase)
+		require.Nil(t, err, `case %v has invalid pattern "%v"`, i, tt.pattern)
+		if tt.match {
+			assert.True(t, actual, `pattern "%v" against "%v" should match`)
+		} else {
+			assert.False(t, actual, `pattern "%v" against "%v" should not match`)
+		}
+	}
 }
