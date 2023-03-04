dd11c4ad078523644e30296713fd66dad289ae9c
HANAI tohru <tohru@reedom.com>
2023-03-04 19:54:14 +0900

    test: add tests

--- pkg/util/import_test.go
+++ pkg/util/import_test.go
@@ -1,6 +1,7 @@
 package util_test
 
 import (
+	"go/types"
 	"testing"
 
 	"github.com/reedom/convergen/pkg/util"
@@ -40,4 +41,51 @@ func main() {
 
 	_, ok := names.LookupName("unknown")
 	assert.False(t, ok)
+}
+
+func TestImportNames_TypeName(t *testing.T) {
+	// Define the source code to test.
+	source := `
+		package main
+
+		import (
+			"fmt"
+			"time"
+		)
+
+		type MyInt int
+
+		var now = time.Now()
+
+		func main() {
+			fmt.Println(now)
+			var x MyInt
+			fmt.Println(x)
+		}
+	`
+	file, _, pkg := loadSrc(t, source)
+
+	// Create an ImportNames object.
+	imports := util.NewImportNames(file.Imports)
+
+	// Test TypeName with basic types.
+	assert.Equal(t, "int", imports.TypeName(types.Typ[types.Int]))
+	assert.Equal(t, "bool", imports.TypeName(types.Typ[types.Bool]))
+	assert.Equal(t, "string", imports.TypeName(types.Typ[types.String]))
+	assert.Equal(t, "float64", imports.TypeName(types.Typ[types.Float64]))
+	assert.Equal(t, "complex128", imports.TypeName(types.Typ[types.Complex128]))
+
+	// Test TypeName with named types in the same package.
+	namedType := pkg.Scope().Lookup("MyInt").Type()
+	assert.Equal(t, "MyInt", imports.TypeName(namedType))
+	assert.False(t, imports.IsExternal(namedType))
+
+	// Test TypeName with named types in external packages.
+	namedType = pkg.Scope().Lookup("now").Type()
+	assert.Equal(t, "time.Time", imports.TypeName(namedType))
+	assert.True(t, imports.IsExternal(namedType))
+
+	path, ok := imports.LookupName("time")
+	assert.True(t, ok)
+	assert.NotEmpty(t, path)
 }
